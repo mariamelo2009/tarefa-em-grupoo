e456383ca271406c20ce81c976d5ef3890a01d71
Lully21 <luisaesteves20@gmail.com>
2026-05-05 15:44:11 -0300

    Update tarefas.js
    alterando titulo e incluida função de concluir tarefa. (feito pelo membro 3)

--- tarefas.js
+++ tarefas.js
@@ -1,6 +1,6 @@
 // tarefas.js - Sistema de Lista de Tarefas
 let tarefas = [];
-let titulo = "GERENCIADOR DE TAREFAS"; // <- linha do conflito
+let titulo = "LISTA DE TAREFAS V2";
 function adicionarTarefa(descricao) {
   tarefas.push({ descricao: descricao, concluida: false });
   console.log("Tarefa adicionada: " + descricao);
@@ -18,3 +18,14 @@ function listarTarefas() {
 // Teste inicial
 adicionarTarefa("Estudar Git");
 listarTarefas();
+
+function concluirTarefa(indice) {
+if (indice < 1 || indice > tarefas.length) {
+console.log("Indice invalido."); return;
+}
+tarefas[indice - 1].concluida = true;
+console.log("Concluida: " + tarefas[indice-1].descricao);
+}
+// Teste
+adicionarTarefa("Tarefa C");
+concluirTarefa(1); listarTarefas();
